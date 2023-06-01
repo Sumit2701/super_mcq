a316f654ff31a3f6bca84eab9c32fafb3fbc8ada
Sumit2701 <sumitahire2701@gmail.com>
2023-06-02 03:09:46 +0530

    nice working with good question skills

--- pages/questions.js
+++ pages/questions.js
@@ -108,8 +108,24 @@ const { Header, Content, Footer } = Layout;
     }
     return '';
   };
-
-
+//  const getOptionStyle=(question, option,questionIndex)=>{
+//   if(submitted[questionIndex]){
+//  return (option==question.correctAnswer)?"":""
+// } }
+const getOptionStyle = (question, option, questionIndex) => {
+  if (submitted[questionIndex]) {
+    return option === question.correctAnswer ? (
+      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 24 24"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg>  
+    ) : null;
+  }
+};
+const getCorrectOptionStyle=(question, option,questionIndex)=> {
+  if (submitted[questionIndex]) {
+    return option === question.correctAnswer ? (
+     styles.optionCorrect
+    ) : '';
+  }
+};
   const getQuestionCardStyle = (questionIndex) => {
     if (showResults[questionIndex]) {
       const selectedOption = questions[questionIndex].options[selectedOptions[questionIndex]];
@@ -152,11 +168,12 @@ const { Header, Content, Footer } = Layout;
                     in={showResults[questionIndex] && selectedOptions[questionIndex] === optionIndex}
                   >
                     <Radio
-                      className={`${styles.option}`}
-                      key={optionIndex}
+                   className={`${styles.option}`}
+                    key={optionIndex}
                       value={optionIndex}
+                    
                     >
-                      {option}
+                    <p className={`${getCorrectOptionStyle(question, option,questionIndex)}`} style={{ display: 'inline' }}>  {option} {getOptionStyle(question, option,questionIndex)}</p>
                     </Radio>
                   </CSSTransition>
                 ))}
